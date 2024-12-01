6815b58934728c7223fab377549413fb72a78298
Kenneth Allen <kenneth@kallen.dev>
2024-12-01 17:26:17 +1100

    Solve day 01

--- src/d01.rb
+++ src/d01.rb
@@ -1,7 +1,10 @@
-
-
 ptn = /^(\d+)\s+(\d+)$/
-
 lines = File.foreach("data/01.txt").map {|line| ptn.match(line)[1..2].map(&:to_i) }
-lists = (0..1).map
 
+lists = (0..1).map {|i| lines.map {|line| line[i] } }
+lists.each(&:sort!)
+puts lists[0].zip(lists[1]).map {|vals| (vals[0] - vals[1]).abs }.sum
+
+l1_counts = Hash.new(0)
+lists[1].each {|n| l1_counts[n] += 1 }
+puts lists[0].map {|n| n * l1_counts[n] }.sum
