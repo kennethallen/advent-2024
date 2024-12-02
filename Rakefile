3a2bbe8f6575e67b936e1e06744e56cf0f1debbb
Kenneth Allen <kenneth@kallen.dev>
2024-12-02 11:50:01 +1100

    Fix test method name

--- Rakefile
+++ Rakefile
@@ -4,5 +4,4 @@ Rake::TestTask.new do |t|
   t.libs << "test"
 end
 
-desc "Run tests"
 task default: :test
--- test/test_day01.rb
+++ test/test_day01.rb
@@ -2,8 +2,10 @@ require 'test/unit'
 require 'day01'
 
 class Day01Test < Test::Unit::TestCase
-  def test
+  def test_sample
     assert_equal(day01(File.foreach("data/01a.txt")), [11, 31])
+  end
+  def test_challenge
     assert_equal(day01(File.foreach("data/01.txt")), [1941353, 22539317])
   end
 end
