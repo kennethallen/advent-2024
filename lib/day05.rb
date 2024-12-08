546cffd364dfcdc5217ee644076cc73c122852cc
Kenneth Allen <kenneth@kallen.dev>
2024-12-09 09:23:24 +1100

    Solve day 05 part 2

--- lib/day05.rb
+++ lib/day05.rb
@@ -1,21 +1,37 @@
+private def score(updates) = updates.sum {|u| u[u.length / 2] }
+
 def day05(lines)
   rules_lines, _, updates_lines = lines.slice_when {|b, a| [b, a].include? "\n" }.to_a
 
   rules = rules_lines.map do |l|
     /^(\d+)\|(\d+)$/.match(l)[1..2].map(&:to_i)
-  end
+  end.to_set
 
   updates = updates_lines.map do |l|
     l.split(',').map(&:to_i)
   end
 
-  [
-    updates.filter do |u|
-      page_nums = Hash[u.each_with_index.to_a]
-      rules.all? do |r|
-        r.map {|n| page_nums[n] }.compact.each_cons(2).all? {|a, b| a < b }
+  right, wrong = updates.partition do |u|
+    page_nums = Hash[u.each_with_index.to_a]
+    rules.all? do |r|
+      r.map {|n| page_nums[n] }.compact.each_cons(2).all? {|a, b| a < b }
+    end
+  end
+
+  wrong.each do |u|
+    loop do
+      changed = false
+      (0...u.length).each do |i|
+        (i+1...u.length).each do |j|
+          if rules.include? [u[j], u[i]]
+            u[i], u[j] = u[j], u[i]
+            changed = true
+          end
+        end
       end
-    end.sum {|u| u[u.length / 2] },
-    0,
-  ]
+      break if !changed
+    end
+  end
+
+  [right, wrong].map {|u| score u }
 end
--- test/test_day05.rb
+++ test/test_day05.rb
@@ -3,9 +3,9 @@ require 'day05'
 
 class Day05Test < Test::Unit::TestCase
   def test_sample
-    assert_equal([143, 0], day05(File.foreach("data/05a.txt")))
+    assert_equal([143, 123], day05(File.foreach("data/05a.txt")))
   end
   def test_challenge
-    assert_equal([4185, 0], day05(File.foreach("data/05.txt")))
+    assert_equal([4185, 4480], day05(File.foreach("data/05.txt")))
   end
 end
