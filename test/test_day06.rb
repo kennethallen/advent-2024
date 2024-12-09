f79dc0c13a31a3b08325ca073af22df8268d2f31
Kenneth Allen <kenneth@kallen.dev>
2024-12-09 15:03:58 +1100

    Allow conditional execution of slow tests

--- test/test_day06.rb
+++ test/test_day06.rb
@@ -6,7 +6,7 @@ class Day06Test < Test::Unit::TestCase
     assert_equal([41, 6], day06(File.foreach("data/06a.txt")))
   end
   def test_challenge
-    omit("slow")
+    omit_unless(ENV["SLOW"] == "1", "slow")
     assert_equal([5131, 1784], day06(File.foreach("data/06.txt")))
   end
 end
