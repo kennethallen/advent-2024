91a96fa3d759fb500a0d70f63315fae1a08cb2af
Kenneth Allen <kenneth@kallen.dev>
2024-12-09 15:15:19 +1100

    Factor out slow test handling

--- test/test_day06.rb
+++ test/test_day06.rb
@@ -1,12 +1,13 @@
 require 'test/unit'
 require 'day06'
+require 'util'
 
 class Day06Test < Test::Unit::TestCase
   def test_sample
     assert_equal([41, 6], day06(File.foreach("data/06a.txt")))
   end
   def test_challenge
-    omit_unless(ENV["SLOW"] == "1", "slow")
+    slow
     assert_equal([5131, 1784], day06(File.foreach("data/06.txt")))
   end
 end
---
+++ test/util.rb
@@ -0,0 +1,1 @@
+def slow = omit_unless(ENV["SLOW"] == "1", "slow")
