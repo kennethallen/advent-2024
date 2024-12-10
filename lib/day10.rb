6baceb1bfa2c4ac956c0bce867f561a566d5cdb6
Kenneth Allen <kenneth@kallen.dev>
2024-12-10 17:45:09 +1100

    Solve day 10 part 2

--- lib/day10.rb
+++ lib/day10.rb
@@ -16,31 +16,57 @@ private def djikstra(start)
   visited
 end
 
+private def djikstra_all(start)
+  to_visit = [[start, []]]
+  visited = Hash.new {|h, k| h[k] = Set.new }
+  while entry = to_visit.pop
+    node, path = entry
+
+    visited[node] << path
+    forward_path = path + [node]
+    (yield node).each do |forward|
+      to_visit << [forward, forward_path]
+    end
+  end
+  visited
+end
+
+private def nexts((y, x), map, (y_limit, x_limit))
+  [
+    ([y-1, x] if y > 0),
+    ([y+1, x] if y+1 < y_limit),
+    ([y, x-1] if x > 0),
+    ([y, x+1] if x+1 < x_limit),
+  ].compact.filter do |ay, ax|
+    map[ay][ax] - map[y][x] == 1
+  end
+end
+
 def day10(lines)
   map = lines.map do |l|
     l[...-1].each_char.map(&:to_i)
   end
-  y_limit, x_limit = map.length, map[0].length
+  dims = map.length, map[0].length
+  trailheads = map.each_with_index.flat_map do |row, y|
+    row.each_with_index.filter_map do |cell, x|
+      [y, x] if cell == 0
+    end
+  end
 
   [
-    map.each_with_index.flat_map do |row, y|
-      row.each_with_index.filter_map do |cell, x|
-        if cell == 0
-          djikstra([y, x]) do |y, x|
-            [
-              ([y-1, x] if y > 0),
-              ([y+1, x] if y+1 < y_limit),
-              ([y, x-1] if x > 0),
-              ([y, x+1] if x+1 < x_limit),
-            ].compact.filter do |ay, ax|
-              map[ay][ax] - map[y][x] == 1
-            end
-          end.count do |(end_y, end_x), _|
-            map[end_y][end_x] == 9
-          end
-        end
+    trailheads.sum do |y, x|
+      djikstra([y, x]) do |pos|
+        nexts(pos, map, dims)
+      end.count do |(end_y, end_x), _|
+        map[end_y][end_x] == 9
       end
-    end.sum,
-    0,
+    end,
+    trailheads.sum do |y, x|
+      djikstra_all([y, x]) do |pos|
+        nexts(pos, map, dims)
+      end.filter do |(end_y, end_x), _|
+        map[end_y][end_x] == 9
+      end.sum {|_, paths| paths.length }
+    end,
   ]
 end
--- test/test_day10.rb
+++ test/test_day10.rb
@@ -4,9 +4,9 @@ require 'util'
 
 class Day10Test < Test::Unit::TestCase
   def test_sample
-    assert_equal([36, 0], day10(File.foreach("data/10a.txt")))
+    assert_equal([36, 81], day10(File.foreach("data/10a.txt")))
   end
   def test_challenge
-    assert_equal([552, 0], day10(File.foreach("data/10.txt")))
+    assert_equal([552, 1225], day10(File.foreach("data/10.txt")))
   end
 end
