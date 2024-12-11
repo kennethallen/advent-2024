661a2078a4ea805751096639437fdef3daf0b694
Kenneth Allen <kenneth@kallen.dev>
2024-12-11 20:24:16 +1100

    Solve day 11

--- lib/day11.rb
+++ lib/day11.rb
@@ -1,6 +1,28 @@
+private def step_stone(s)
+  if s == 0
+    [1]
+  elsif (num_digits = Math::log10(s).next_float.ceil).even?
+    s.divmod 10**(num_digits/2)
+  else
+    [s*2024]
+  end
+end
+
 def day11(lines)
+  stones = Hash.new 0
+  lines.first.split(" ").each {|s| stones[s.to_i] += 1 }
+
+  stones25 = nil
+  (0...75).each do |i|
+    stones25 = stones if i == 25
+    stones_old, stones = stones, Hash.new(0)
+    stones_old.each do |s, n|
+      step_stone(s).each {|s| stones[s] += n }
+    end
+  end
+
   [
-    0,
-    0,
+    stones25.values.sum,
+    stones.values.sum,
   ]
 end
--- test/test_day11.rb
+++ test/test_day11.rb
@@ -3,9 +3,9 @@ require 'day11'
 
 class Day11Test < Test::Unit::TestCase
   def test_sample
-    assert_equal([0, 0], day11(File.foreach("data/11a.txt")))
+    assert_equal([55312, 65601038650482], day11(File.foreach("data/11a.txt")))
   end
   def test_challenge
-    assert_equal([0, 0], day11(File.foreach("data/11.txt")))
+    assert_equal([175006, 207961583799296], day11(File.foreach("data/11.txt")))
   end
 end
