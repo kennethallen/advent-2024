9aaa08b0035703159426e0544d3601b888e9d3f5
Kenneth Allen <kenneth@kallen.dev>
2024-12-13 20:11:25 +1100

    Solve day 13 part 1 naively

--- lib/day13.rb
+++ lib/day13.rb
@@ -1,6 +1,26 @@
+private def parse13(ptn, s)
+  ptn.match(s)[1..].map(&:to_i)
+end
+
 def day13(lines)
+  machines = lines.slice_after(/^$/).map do |but_a, but_b, prize|
+    [
+      parse13(/Button A: X\+(\d+), Y\+(\d+)/, but_a),
+      parse13(/Button B: X\+(\d+), Y\+(\d+)/, but_b),
+      parse13(/Prize: X=(\d+), Y=(\d+)/, prize),
+    ]
+  end
+
   [
-    0,
+    machines.sum do |(ax, ay), (bx, by), (px, py)|
+      (0..100).flat_map do |a|
+        (0..100).map {|b| [a, b] }
+      end.filter do |a, b|
+        a*ax + b*bx == px && a*ay + b*by == py
+      end.map do |a, b|
+        3*a + b
+      end.min || 0
+    end,
     0,
   ]
 end
