87bc9e9dbb70d8d52fed0c99e4096d3ed8d92321
Kenneth Allen <kenneth@kallen.dev>
2024-12-14 22:04:11 +1100

    Start solving day 14 part 1

--- lib/day14.rb
+++ lib/day14.rb
@@ -1,4 +1,11 @@
 def day14(lines)
+  dims = /(\d+) (\d+)/.match(lines.first)[1..].map(&:to_i)
+  robots = lines[1..].map do |l|
+    /p=(-?\d+),(-?\d+) v=(-?\d+),(-?\d+)/.match(l)[1..].map(&:to_i)
+  end
+
+  p dims, robots
+
   [
     0,
     0,
--- test/test_day14.rb
+++ test/test_day14.rb
@@ -3,7 +3,7 @@ require 'day14'
 
 class Day14Test < Test::Unit::TestCase
   def test_sample
-    assert_equal([0, 0], day14(File.foreach("data/14a.txt")))
+    assert_equal([12, 0], day14(File.foreach("data/14a.txt")))
   end
   def test_challenge
     assert_equal([0, 0], day14(File.foreach("data/14.txt")))
