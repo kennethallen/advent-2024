c23970d4bf4169848b0cfa7a59ba5487a390b229
Kenneth Allen <kenneth@kallen.dev>
2024-12-16 09:54:18 +1100

    Solve day 15 part 1

--- lib/day15.rb
+++ lib/day15.rb
@@ -1,6 +1,64 @@
+def step15((y, x), move, (y_limit, x_limit))
+  case move
+  when "<"
+    [y, x-1] if x > 0
+  when ">"
+    [y, x+1] if x+1 < x_limit
+  when "^"
+    [y-1, x] if y > 0
+  when "v"
+    [y+1, x] if y+1 < y_limit
+  end
+end
+
 def day15(lines)
+  map, moves = lines.slice_after(/^$/).map(&:to_a)
+  robot = nil
+  map = map[1...-2].each_with_index.map do |r, y|
+    r[1...-2].each_char.each_with_index.map do |c, x|
+      case c
+      when "O"
+        :box
+      when "#"
+        :wall
+      when "@"
+        robot = [y, x]
+        nil
+      end
+    end
+  end
+  moves = moves.map {|m| m[...-1] }.join
+  dims = [map.length, map[0].length]
+
+  moves.each_char do |move|
+    dest = robot
+    first_step = nil
+    while (dest = step15(dest, move, dims)) && map[dest[0]][dest[1]] != :wall
+      first_step ||= dest
+      unless map[dest[0]][dest[1]]
+        map[dest[0]][dest[1]] = :box
+        map[first_step[0]][first_step[1]] = nil
+        robot = first_step
+        break
+      end
+    end
+=begin
+    map.each_with_index do |r, y|
+      r.each_with_index do |c, x|
+        print case when robot == [y, x] then "@" when c == :box then "O" when c == :wall then "#" else "." end
+      end
+      puts
+    end
+    puts
+=end
+  end
+
   [
-    0,
+    map.each_with_index.flat_map do |r, y|
+      r.each_with_index.map do |c, x|
+        x+1 + 100*(y+1) if c == :box
+      end
+    end.compact.sum,
     0,
   ]
 end
--- test/test_day15.rb
+++ test/test_day15.rb
@@ -3,9 +3,10 @@ require 'day15'
 
 class Day15Test < Test::Unit::TestCase
   def test_sample
-    assert_equal([0, 0], day15(File.foreach("data/15a.txt")))
+    assert_equal([2028, 0], day15(File.foreach("data/15a.txt")))
+    assert_equal([10092, 0], day15(File.foreach("data/15b.txt")))
   end
   def test_challenge
-    assert_equal([0, 0], day15(File.foreach("data/15.txt")))
+    assert_equal([1349898, 0], day15(File.foreach("data/15.txt")))
   end
 end
