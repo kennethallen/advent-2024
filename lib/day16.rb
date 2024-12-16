ed43d117c03b7c0c9de7535a6f529fd1eccf7559
Kenneth Allen <kenneth@kallen.dev>
2024-12-16 19:33:05 +1100

    Solve day 16 part 1

--- lib/day16.rb
+++ lib/day16.rb
@@ -1,6 +1,55 @@
+require 'util'
+
 def day16(lines)
+  t_start = nil
+  t_end = nil
+  walls = lines.each_with_index.map do |l, y|
+    l[...-1].each_char.each_with_index.map do |c, x|
+      case c
+      when "#"
+        :wall
+      when "S"
+        t_start = [y, x]
+        nil
+      when "E"
+        t_end = [y, x]
+        nil
+      end
+    end
+  end
+
+  paths = djikstra([:e, t_start]) do |dir, (y, x)|
+    nexts = [
+      case dir
+      when :e then :n
+      when :n then :w
+      when :w then :s
+      when :s then :e
+      end,
+      case dir
+      when :e then :s
+      when :n then :e
+      when :w then :n
+      when :s then :w
+      end,
+    ].map {|d| [1_000, [d, [y, x]]] }
+    sy, sx = case dir
+    when :e
+      [y, x+1]
+    when :w
+      [y, x-1]
+    when :n
+      [y-1, x]
+    when :s
+      [y+1, x]
+    end
+    nexts << [1, [dir, [sy, sx]]] unless walls[sy][sx]
+    nexts
+  end
+
   [
-    0,
+    %i(n e s w).map {|dir| paths[[dir, t_end]][0] }.min,
     0,
   ]
 end
+
--- test/test_day16.rb
+++ test/test_day16.rb
@@ -3,9 +3,10 @@ require 'day16'
 
 class Day16Test < Test::Unit::TestCase
   def test_sample
-    assert_equal([0, 0], day16(File.foreach("data/16a.txt")))
+    assert_equal([7036, 0], day16(File.foreach("data/16a.txt")))
+    assert_equal([11048, 0], day16(File.foreach("data/16b.txt")))
   end
   def test_challenge
-    assert_equal([0, 0], day16(File.foreach("data/16.txt")))
+    assert_equal([107512, 0], day16(File.foreach("data/16.txt")))
   end
 end
