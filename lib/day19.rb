ecab5828df5bed873e1cec2e25c690aaba3f0add
Kenneth Allen <kenneth@kallen.dev>
2024-12-27 11:49:34 +1100

    Solve day 19 part 1

--- lib/day19.rb
+++ lib/day19.rb
@@ -1,6 +1,33 @@
+require 'util'
+
+class Pather19
+  include AStar
+
+  def initialize(towels)
+    @towels = towels
+  end
+
+  def terminal?(pos) = pos.empty?
+
+  def heuristic(pos) = pos.length
+
+  def nexts(state)
+    @towels.filter_map do |t|
+      [t.length, state[t.length..]] if t == state[...t.length]
+    end
+  end
+end
+
 def day19(lines)
+  towels, _, *designs = lines.to_a
+  towels = towels[...-1].split(/, /)
+  designs = designs.map {|d| d[...-1] }
+  pather = Pather19.new towels
+
   [
-    0,
+    designs.count do |d|
+      pather.path(d)[0]
+    end,
     0,
   ]
 end
--- test/test_day19.rb
+++ test/test_day19.rb
@@ -3,9 +3,10 @@ require 'day19'
 
 class Day19Test < Test::Unit::TestCase
   def test_sample
-    assert_equal([0, 0], day19(File.foreach("data/19a.txt")))
+    assert_equal([6, 0], day19(File.foreach("data/19a.txt")))
   end
   def test_challenge
-    assert_equal([0, 0], day19(File.foreach("data/19.txt")))
+    slow
+    assert_equal([317, 0], day19(File.foreach("data/19.txt")))
   end
 end
